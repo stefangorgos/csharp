f237bc8b1f83d106953625478b91a0771f15ed08
stefangorgos <stefan.gorgos.dev@gmail.com>
2024-01-18 23:12:15 +0200

    subsequence extraction

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        static string GetSubsequences(string sequence)
+        {
+            int start = 0;
+            int end = start + 1;
+            List<string> ssequence = new List<string>();
+            while (start < sequence.Length)
+            {
+                if (end < sequence.Length && sequence[end - 1] != sequence[end])
+                {
+                    ssequence.Add(sequence.Substring(start, end - start + 1));
+                    end++;
+                }
+                else
+                {
+                    start++;
+                    end = start + 1;
+                }
+            }
+            return String.Join(", ", ssequence.ToArray());
+        }
         static void Main(string[] args)
-        {           
-
+        {
+            Console.WriteLine("enter your character sequence");
+            string seq = Console.ReadLine();
+            string result = GetSubsequences(seq);
+            Console.WriteLine(result);
         }
     }
 }
