8d5d583e6674b13ad1d5a94c551a1f526e88afde
stefangorgos <stefan.gorgos.dev@gmail.com>
2024-01-18 21:52:42 +0200

    chessboard cell color determination

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Task1
+﻿using System.Text.RegularExpressions;
+
+namespace Task1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static string GetColor(string cell)
         {
-            Console.WriteLine("Hello, World!");
+            if (Regex.IsMatch(cell, "^[A-Ha-h][1-8]$"))
+            {
+                return (cell[0] + cell[1]) % 2 == 1 ? "white" : "black";  
+            }
+            return "invalid cell format";
+
+        }
+
+        static void Main(string[] args)
+        {           
+            Console.Write("enter the desired cell: ");
+            string cell = Console.ReadLine();
+            string result = GetColor(cell);
+            Console.WriteLine(result);
         }
     }
 }
